635f3ffe8c2ef8007d4bb2036838b6e104a70ef4
Adil Hernandez <adil.hernandez91@gmail.com>
2019-02-17 19:47:50 +0100

    Added routes to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 import './pages/auth.dart';
+import './pages/manage_products.dart';
+import './pages/products.dart';
+import './pages/product.dart';
 
 
 void main() {
-
   runApp(MyApp());
 }
 
@@ -18,7 +20,26 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.deepOrange,
         accentColor: Colors.deepPurple,
       ),
-      home: AuthPage(),
+      // home: AuthPage(),
+      routes: {
+        '/': (BuildContext context) => ProductsPage(),
+        '/admin': (BuildContext context) => ManageProducts(),
+      },
+      onGenerateRoute: (RouteSettings settings) {
+        final List<String> pathElements = settings.name.split('/');
+        if (pathElements[0] != '') {
+          return null;
+        }
+        if (pathElements[1] == 'product') {
+          final int index = int.parse(pathElements[2]);
+
+          return MaterialPageRoute(
+                        builder: (BuildContext context) => ProductPage(
+                            products[index]['title'], products[index]['image']),
+                      );
+        }
+        return null;
+      },
     );
   }
 }
